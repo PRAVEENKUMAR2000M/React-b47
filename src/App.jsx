dd23f7af7b11c35c8ed965e6b74305654e873e4a
PRAVEENKUMAR2000M git config --global user.PRAVEENKUMAR2000M git config --global user.name PRAVEENKUMAR2000M <praveenmech483@gmail.com>
2023-10-08 13:34:25 +0530

    multiple copy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-const App = () => {
+// const App = () => {
+
+//     const a = 10;
+//     const b = 20;
+//     console.log(a + b);
+//     const now = new Date(); 
+//     return (
+//         <div>    
+//          <p>hello world now time is {now.toString()}</p>
+//             <p>the {a} plus {b} is {a+b}</p>
+//       </div>
+//   ) 
+// }
+// export default App;
 
 
-    const now = new Date();
 
-    console.log("hello world");
-    
+
+
+
+const Hello = () => {
     return (
         <div>
-            
-            <p>hello world now time is { now.toString() }</p>
-      </div>
-  ) 
+            <p>
+                hello world
+            </p>
+        </div>
+    )
 }
+
+const App = () => {
+    return (
+        <div>
+        <h1>greetings</h1>
+            <Hello />
+            </div>
+    )
+}
+
+
 export default App;
