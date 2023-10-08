db36d60b6e66ae7424d11e2e52498892712e3fad
PRAVEENKUMAR2000M git config --global user.PRAVEENKUMAR2000M git config --global user.name PRAVEENKUMAR2000M <praveenmech483@gmail.com>
2023-10-08 12:12:43 +0530

    default code simplification

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 const App = () => {
 
+
+    const now = new Date();
+
     console.log("hello world");
+    
     return (
         <div>
-            <p>hello world</p>
+            
+            <p>hello world now time is { now.toString() }</p>
       </div>
   ) 
 }
