f39bfc35bc5d829376f1e08a72941c4426201574
PRAVEENKUMAR2000M git config --global user.PRAVEENKUMAR2000M git config --global user.name PRAVEENKUMAR2000M <praveenmech483@gmail.com>
2023-10-08 14:24:18 +0530

    dynamic operator

--- src/App.jsx
+++ src/App.jsx
@@ -18,24 +18,63 @@
 
 
 
-const Hello = () => {
+// const Hello = () => {
+//     return (
+//         <div>
+//             <p>
+//                 hello world
+//             </p>
+//         </div>
+//     )
+// }
+
+// const App = () => {
+//     return (
+//         <div>
+//         <h1>greetings</h1>
+//             <Hello />
+//             </div>
+//     )
+// }
+
+
+// export default App;
+
+
+
+
+
+
+
+
+const Hello = (props) => {
+    console.log(props);
     return (
         <div>
-            <p>
-                hello world
-            </p>
+            <p>hello { props.name }, your age is { props.age }</p>
         </div>
     )
 }
 
+
 const App = () => {
     return (
         <div>
-        <h1>greetings</h1>
-            <Hello />
-            </div>
+            <h1 style={{color:"white", backgroundColor:"red"}}>greetings</h1>
+            <Hello name="praveen" age="23" />
+        </div>
     )
 }
 
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
