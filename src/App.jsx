3d0e7a4efc6168a747af20f71bca8f45d9d33162
PRAVEENKUMAR M <131171871+PRAVEENKUMAR2000M@users.noreply.github.com>
2023-10-27 11:14:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -53,3 +53,60 @@ return(
 }
 
 export default App
+
+
+
+
+
+import React, {Component} from "react";
+
+class App extends Component{
+
+constructor(props){
+super(props);
+
+this.state = {
+goodCount: 0,
+neutralCount: 0,
+badCount: 0,
+};
+}
+
+good = () =>{
+this.setState((prevState)=>({
+goodCount: prevState.goodCount +1,
+}));
+}
+
+neutral = () =>{
+this.setState((prevState)=>({
+neutralCount: prevState.neutralCount +1,
+}));
+}
+
+bad = () =>{
+this.setState((prevState)=>({
+badCount: prevState.badCount +1,
+}));
+}
+
+render(){
+return(
+<div>
+<h1>Give feedback</h1>
+<button onClick={this.good}>good</button>
+<button onClick={this.neutral}>neutral</button>
+<button onClick={this.bad}>bad</button>
+<h1>statistics</h1>
+<p>good: {this.state.goodCount}</p>
+<p>neutral: {this.state.neutralCount}</p>
+<p>bad: {this.state.badCount}</p>
+</div>
+);
+}
+}
+
+export default App
+
+
+
